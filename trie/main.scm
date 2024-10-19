7253dca116ff508871826db7e3c1a4aca6fa786e
sebastian <sebaslogo@gmail.com>
2024-10-18 20:34:57 -0500

    scheme too hard, need to switch to Racket

--- trie/main.scm
+++ trie/main.scm
@@ -1,11 +1,19 @@
+(define (range start end)
+   (if (= start end)
+      (list start)
+      (append (range start (- end 1)) (list end))))
+
 ; Hash function for characters a-Z
 (define hash
     (lambda (char) (- (char->integer (char-upcase char)) 65 )))
 
+(define unhash
+    (lambda (num) (char-upcase (integer->char (+ num 65)))))
+
 ; Node constructor 
-; ( list[False], True)
+; ( list[Node | False], Boolean )
 (define make-node   
-    (lambda () (cons (make-vector 26 #f) #t)))
+    (lambda () (cons (make-vector 26 #f) #f)))
 
 ; Add a child node to an existing node, for a specified character
 (define insert-char
@@ -26,36 +34,58 @@
 ; Internal method
 (define insert-string-i
     (lambda (string trie-node index)
-        ; if string is empty
         (if (= index (string-length string))
+            ; string is empty
             (begin
-                (display "Inserted: ")
-                (display string)
+                (set-cdr! trie-node #t)
                 (newline)
             )
-            (if (get-node-from-char (string-ref string index) trie-node)
-                ; If node is not false
-                (begin
-                    (display "Node is not terminal. \n")
-                    (display (string-ref string index))
-                    (newline)
-                    (insert-string-i string (get-node-from-char (string-ref string index) trie-node) (+ index 1))
-                )
-                ; If node is false
-                (begin
-                    (display "Node is terminal. \n")
-                    (display (string-ref string index))
-                    (newline)
+            (begin
+                (display (string-ref string index))
+                ; Insert node if it does not exist
+                (if (not (get-node-from-char (string-ref string index) trie-node))
                     (insert-char (string-ref string index) trie-node)
-                    (set-cdr! trie-node #f)
-                    (insert-string-i string (get-node-from-char (string-ref string index) trie-node) (+ index 1))
                 )
+                (insert-string-i string (get-node-from-char (string-ref string index) trie-node) (+ index 1))
             )
         )
     )
 )
 
+; Print all strings in trie
+(define get-all-strings-in-trie
+    (lambda (trie-node) (get-all-strings-i "" trie-node)))
+
+; Helper
+(define get-all-strings-i
+    (lambda (string trie-node)
+        (if (cdr trie-node)
+            string ; node is terminal, return list of string
+                (map ; for all child nodes
+                    (lambda (index)
+                        (let ((child-node (get-node-from-char (unhash index) trie-node)))
+                            (if child-node
+                                (get-all-strings-i 
+                                    (string-append string (make-string 1 (unhash index))) ; string + char
+                                    child-node
+                                )
+                                ""
+                            )
+                        )
+                    )
+                    (range 0 25)
+                )
+        )
+    )
+)
+
+; TODO: check if string is in trie
+; TODO: return all strings that match string
+
+
+
 (define my-trie (make-node))
 
 (insert-string "HATS" my-trie)
 (insert-string "HATCH" my-trie)
+(get-all-strings-in-trie my-trie)
