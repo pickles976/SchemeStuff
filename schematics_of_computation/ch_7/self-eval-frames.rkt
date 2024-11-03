6972c7507b82b89b16306ca975d5c65ae5898f6f
sebastian <sebaslogo@gmail.com>
2024-11-03 08:34:13 -0600

    got environment stuff working

--- schematics_of_computation/ch_7/self-eval-frames.rkt
+++ schematics_of_computation/ch_7/self-eval-frames.rkt
@@ -22,21 +22,42 @@
                         (evaluator-error 
                             "Too few arguments!" (cons names values))
                         (if (and (pair? n) (symbol? (car n))) ; if n is a list, and the first element is a symbol
-                            (cons                      ; construct a list like: ((name . value) (name . value) '()) (this is an env)   
-                                (mcons (car n) (car v)) 
+                            (mcons                      ; construct a mutable list like: ((name . value) (name . value) '()) (this is an env)   
+                                (mcons (car n) (car v))  
                                 (new-frame (cdr n) (cdr v)))
                             (evaluator-error 
                                 "Invalid parameter list!" names))))))])
-        (cons (new-frame names values) env))) ; append our new env to the list of envs
+        (mcons (new-frame names values) env))) ; append our new env to the list of envs
 
 ; use assv to check all environments to find a variable
 (define (find-binding symb env)
     (if (null? env)
         #f ; all environments traversed and no binding found
-        (let ((x (assv symb (car env)))) ; see if there is an item in the env with the same name as 'symb'
+        (let ((x (massv symb (mcar env)))) ; see if there is an item in the env with the same name as 'symb'
             (if (not x)
-                (find-binding symb (cdr env)) ; recurse if not found
+                (find-binding symb (mcdr env)) ; recurse if not found
                 x))))
+
+; get the value of a variable
+(define (get-binding symb env)
+    (let ([x (find-binding symb env)])
+        (if (not x )
+            (evaluator-error "Unbound variable (get)" symb)
+            (mcdr x))))
+
+; set the value of a binding
+(define (set-binding symb val env)
+    (let ([x (find-binding symb env)])
+        (if (not x)
+            (evaluator-error "Unbound variable (set)" symb)
+            (set-mcdr! x val))))
+
+; 
+(define (def-binding symb val env)
+    (let ([x (massv symb (mcar env))])
+        (if (not x)
+            (set-mcar! env (mcons (mcons symb val) (mcar env)))
+            (set-mcdr! x val))))
 
 
 (define (evaluator-error message items)
@@ -46,9 +67,18 @@
     (newline)
 )
 
-(define root-env (extend-environment (list 'x) (list 1) '()))
-(print root-env)
+(define root-env (extend-environment (list 'x) (list 1) '())) ; Create a root env with binding x=1
+(print root-env) 
 (newline)
-(define child-env (extend-environment (list 'y 'z) (list 2 3) root-env))
+
+(define child-env (extend-environment (list 'y 'z) (list 2 3) root-env)) ; append a sub env with y=2 and z=3
+(print child-env)
+(newline)
+
+(def-binding 'a 10 child-env) ; add a=10 to child env
+(print child-env)
+(newline)
+
+(set-binding 'a 12 child-env) ; set a=12
 (print child-env)
 (newline)
