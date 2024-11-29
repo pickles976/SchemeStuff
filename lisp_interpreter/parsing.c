0aded7a48558f7c3e0625399b2fc1c7f2265fd6e
pickles976 <sebaslogo@gmail.com>
2024-11-29 13:45:14 -0600

    Finished ch6

--- lisp_interpreter/parsing.c
+++ lisp_interpreter/parsing.c
@@ -27,14 +27,41 @@ void add_history(char *unused) {}
 #endif
 
 int main(int argc, char **argv) {
+
+  mpc_parser_t *Number = mpc_new("number");
+  mpc_parser_t *Operator = mpc_new("operator");
+  mpc_parser_t *Expr = mpc_new("expr");
+  mpc_parser_t *Lispy = mpc_new("lispy");
+
+  /* Define them with the following Language */
+  mpca_lang(MPCA_LANG_DEFAULT,
+            "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      lispy    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+            Number, Operator, Expr, Lispy);
+
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl+C to Exit\n");
 
   while (1) {
     char *input = readline("lispy> ");
     add_history(input);
-    printf("No you're a %s", input);
+
+    mpc_result_t r;
+    // filename, string, parser, result
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+
     free(input);
   }
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   return 0;
 }
