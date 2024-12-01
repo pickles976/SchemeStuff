be21bd6a60516d264023eb352eb4fd17210c85fc
Sebastian Gomez <sebaslogo@gmail.com>
2024-12-01 15:00:36 -0600

    Finished ch 09

--- lisp_interpreter/README.md
+++ lisp_interpreter/README.md
@@ -5,3 +5,7 @@ Working through projet from here:
 https://www.buildyourownlisp.com/
 
 https://www.buildyourownlisp.com/chapter9_s_expressions
+
+- [ ] read about memmove
+- [ ] add a remainder operator
+- [ ] add a double field?
--- lisp_interpreter/ch_09.c
+++ lisp_interpreter/ch_09.c
@@ -29,7 +29,7 @@ void add_history(char *unused) {}
 enum { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR };
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
-typedef struct {
+typedef struct lval {
   int type;
   long num;
   char* err;
@@ -45,7 +45,7 @@ lval* lval_num(long x) {
   return v;
 }
 
-lval* lval_err(char m) {
+lval* lval_err(char* m) {
   lval* v = malloc(sizeof(lval));
   v->type = LVAL_ERR;
   v->err = malloc(strlen(m) + 1);
@@ -144,50 +144,107 @@ void lval_print(lval* v) {
 /* Print an "lval" followed by a newline */
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
-// lval eval_op(lval x, char *op, lval y) {
+lval* lval_pop(lval* v, int i) {
+  lval* x = v->cell[i];
+  // shift memory back
+  memmove(&v->cell[i], &v->cell[i+1], sizeof(lval*) * (v->count - i - 1));
+  v->count--;
+  v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+  return x;
+}
 
-//   if (x.type == LVAL_ERR) {
-//     return x;
-//   }
+lval* lval_take(lval* v, int i) {
+  lval* x = lval_pop(v, i);
+  lval_del(v);
+  return x;
+}
 
-//   if (strcmp(op, "+") == 0) {
-//     return lval_num(x.num + y.num);
-//   }
-//   if (strcmp(op, "-") == 0) {
-//     return lval_num(x.num - y.num);
-//   }
-//   if (strcmp(op, "*") == 0) {
-//     return lval_num(x.num * y.num);
-//   }
-//   if (strcmp(op, "/") == 0) {
-//     return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
-//   }
-//   return lval_err(LERR_BAD_OP);
-// }
+lval* builtin_op(lval* a, char* op) {
+  // make sure all args are numbers
+  for (int i = 0; i < a->count; i++) {
+    if (a->cell[i]->type != LVAL_NUM) {
+      lval_del(a);
+      return lval_err("Cannot operate on a non-number!");
+    }
+  }
 
-// lval eval(mpc_ast_t *t) {
+  lval* x = lval_pop(a, 0);
 
-//   /* If tagged as a number, return directly */
-//   if (strstr(t->tag, "number")) {
-//     errno = 0;
-//     long x = strtol(t->contents, NULL, 10);
-//     return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
-//   }
+  // unary negation
+  if ((strcmp(op, "-") == 0) && a->count == 0) {
+    x->num = -x->num;
+  }
 
-//   /* operator is always second child, first is open paren */
-//   char *op = t->children[1]->contents;
+  /* While there are still elements remaining */
+  while (a->count > 0) {
 
-//   /* third child goes in x */
-//   lval x = eval(t->children[2]);
+    /* Pop the next element */
+    lval* y = lval_pop(a, 0);
 
-//   /* apply and reduce remaining children */
-//   int i = 3;
-//   while (strstr(t->children[i]->tag, "expr")) {
-//     x = eval_op(x, op, eval(t->children[i]));
-//     i++;
-//   }
-//   return x;
-// }
+    if (strcmp(op, "+") == 0) { x->num += y->num; }
+    if (strcmp(op, "-") == 0) { x->num -= y->num; }
+    if (strcmp(op, "*") == 0) { x->num *= y->num; }
+    if (strcmp(op, "/") == 0) {
+      if (y->num == 0) {
+        lval_del(x); lval_del(y);
+        x = lval_err("Division By Zero!"); break;
+      }
+      x->num /= y->num;
+    }
+
+    lval_del(y);
+  }
+
+  lval_del(a); 
+  return x;
+
+}
+
+lval* lval_eval(lval* v);
+
+lval* lval_eval_sexpr(lval* v) { 
+
+  // Evaluate children
+  for (int i = 0; i<v->count;i++) {
+    v->cell[i] = lval_eval(v->cell[i]);
+  }
+
+  for (int i = 0; i < v->count; i++) {
+    if (v->cell[i]->type == LVAL_ERR) {
+      return lval_take(v, i);
+    }
+  }
+
+  // empty
+  if (v->count == 0) {
+    return v;
+  }
+
+  // single expression
+  if (v->count == 1) {
+    return lval_take(v, 0);
+  }
+
+  // Expression must start wiht symbol
+  lval* f = lval_pop(v, 0); 
+  if (f->type != LVAL_SYM) {
+    lval_del(f); lval_del(v);
+    return lval_err("S-expression Does not start with a symbol!");
+  }
+
+  // Call builtin with operator
+  lval* result = builtin_op(v, f->sym);
+  lval_del(f);
+  return result;
+}
+
+lval* lval_eval(lval* v) {
+  if (v->type == LVAL_SEXPR) {
+    return lval_eval_sexpr(v);
+  }
+  return v;
+}
+
 
 int main(int argc, char **argv) {
 
@@ -218,7 +275,7 @@ int main(int argc, char **argv) {
     mpc_result_t r;
     // filename, string, parser, result
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      lval* x = lval_read(r.output);
+      lval* x = lval_eval(lval_read(r.output));
       lval_println(x);
       lval_del(x);
     } else {
